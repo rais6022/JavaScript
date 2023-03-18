d4be6cded7293fd678b0eb4dd4706471a154d8e9
Rais Ahmed Khokhar <raiskhokhar022@gmail.com>
2023-03-18 20:06:49 +0530

    Add these files

--- 06_expression.js
+++ 06_expression.js
@@ -1,25 +1,26 @@
 // 77; // agarr haum ese aise likhte hai 
-// "Rais"  // yeh dono apne aap me ek javascript program hai
-// false 
+// "Rais"  // yeh dono apne aap me ek javascript program hai 
+// binod this is nothing we cannot write it as the binod without using the quotations
+// false  // yeh apne aap me ek boolean litral jhai yeh apne aap me ek js program hai
 console.log("Operators in javascript")
 
 // Arithmetic operator 
 
 let a = 10;
 let b = 4;
-console.log("a + b = " , a+b)
-console.log("a - b = " , a-b)
-console.log("a * b = " , a*b)
-console.log("a / b = " , a/b) // we did not require any float or anything likewise in c++ or java
-console.log("a % b = " , a%b)
-console.log("a ** b = " , a**b)
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a * b = ", a * b)
+console.log("a / b = ", a / b) // we did not require any float or anything likewise in c++ or java
+console.log("a % b = ", a % b) // this means it will show the remainder 
+console.log("a ** b = ", a ** b) // this means it will show the a ki power b for eg 10^4= 
 
-console.log("++a = " , ++a)
-console.log("a++ = " , a++)
-console.log("--a = " , --a)
-console.log("a-- = " , a--)
-console.log("a = " , a)
-console.log("a-- = " , a--)
+console.log("++a = ", ++a)
+console.log("a++ = ", a++)
+console.log("--a = ", --a)
+console.log("a-- = ", a--)
+console.log("a = ", a)
+console.log("a-- = ", a--)
 
 // Assignment operator
 
@@ -36,6 +37,23 @@ console.log("assignment is now = ", assignment)
 // Comparison operator
 
 let comp1 = 6;
-let comp2 = 7;
+let comp2 = "6"; // if this is the string like "6" the === says that it is false 
 console.log("comp1 == comp2 is ", comp1 == comp2)
-console.log("comp1 == comp2 is ", comp1 == comp2)
+console.log("comp1 != comp2 is ", comp1 != comp2)
+console.log("comp1 === comp2 is ", comp1 === comp2) // this will show the typw of the no.'s and the values
+console.log("comp1 !== comp2 is ", comp1 !== comp2)
+
+// Logical operator
+
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!true)
+console.log(!false)
+
+// comments 
+// this is a single line comment
+/* This is a multhi line comment and a tutorial
+from 
+code with harry */
