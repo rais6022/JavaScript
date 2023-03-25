663485b38a239e5357b338eefed51b84fb71dc5b
Rais Ahmed Khokhar <raiskhokhar022@gmail.com>
2023-03-25 15:01:36 +0530

    This file have been modified

--- 13_Strings.js
+++ 13_Strings.js
@@ -1,7 +1,7 @@
-let name = "Rais"
+let name = "Rais" // double quote se bani hui string ko double quote se hi band karoge or single ko single se
 console.log(name)
 console.log(name.length) // by this we can know the length of a string
-console.log(name[0])
+console.log(name[0])  // yeh batayega k what character at 0 index in the string
 console.log(name[1])
 
 
@@ -14,6 +14,18 @@ console.log(friend)
 // Template literals
 let boy1 = "Pramod"
 let boy2 = "Nikhil"
-// Nikhil is a friend of pramod
-let sentence = `${boy2} is a friend of ${boy1}`
+// Nikhil is a friend of pramod  // mai ese likhna chahta hoon
+// let sentence = `boy2 is a friend of boy1`  // agar mai yeh karu to string as it is print ho jayegi
+let sentence = `${boy2} is a friend of ${boy1}`  //  
 console.log(sentence)
+
+// Escape Sequence Characters
+
+// let fruit = 'Bana\'na'  // in this we are using single qute escape sequence character
+// console.log(fruit)
+let fruit = "Bana\"na"  // in this we are using single qute escape sequence character
+console.log(fruit)
+console.log(fruit.length) // if i do .length then the length is only of 7 not 8
+
+
+// Done!
