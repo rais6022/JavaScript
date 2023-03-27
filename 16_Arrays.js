5426e498b799584246e174573356e3beb9541074
Rais Ahmed Khokhar <raiskhokhar022@gmail.com>
2023-03-27 11:43:50 +0530

    ok

--- 16_Arrays.js
+++ 16_Arrays.js
@@ -9,7 +9,7 @@ console.log(marks_class_12[6])
 console.log(marks_class_12[7]) // This will be undefined because index 7 does not exist
 console.log("The length of marks_class_12 is" , marks_class_12.length)
 marks_class_12[7] = 89 // Adding a new value to the array
-marks_class_12[0] = 96 // Changing the value to the array
+marks_class_12[0] = 96 // Changing the value of an array
 console.log(marks_class_12)
 
 // This is the quick quiz: print all elemnts of arry by using for loop
