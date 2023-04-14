135ff44202cd63ca0425e846e81ed81e91846a3d
Rais Ahmed Khokhar <raiskhokhar022@gmail.com>
2023-04-14 20:06:07 +0530

    modified

--- 32_Children/script.js
+++ 32_Children/script.js
@@ -1,4 +1,8 @@
 console.log(document.body.firstChild)
 console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+
+
+
 let arr = Array.from(document.body.childNodes)
 console.log(arr)
