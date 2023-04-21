e68db6c0a97270c95e40c5c613d27522c3711a32
Rais Ahmed Khokhar <raiskhokhar022@gmail.com>
2023-04-21 20:22:23 +0530

    commit

---
+++ 33_sibling_and_parents/script.js
@@ -0,0 +1,7 @@
+console.log("hello world")
+console.log(document.body.firstChild)
+a = document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+
+console.log(a.firstChild.nextSibling) // humne second milega ..  if we compress all spaces and all stuff
