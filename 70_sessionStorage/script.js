4d3e04ea47cdcc7b4fe6e8dbcb010bde5221fb0b
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-05-26 11:22:00 +0530

    sessionStorage

---
+++ 70_sessionStorage/script.js
@@ -0,0 +1,14 @@
+// sessionStorage.setItem("name", "harry")
+
+// sessionStorage.getItem("name")
+
+// sessionStorage.removeItem("name")
+
+// sessionStorage.clear()
+
+
+
+window.onstorage = (e) => {
+    alert("changed")
+    console.log(e)
+} 
