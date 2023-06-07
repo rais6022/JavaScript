17f0be8a29118d3688400531225555d763585b7d
Rais Ahmed Khokhar <raisak6022@gmail.com>
2023-06-07 10:58:41 +0530

    Quotion_is_added

--- 73_Exercise6/script.js
+++ 73_Exercise6/script.js
@@ -1,1 +1,5 @@
 // make an todo app
+
+
+// Create a todo list app capable of storing your todos in localStorage add an option to create delete and access all the todos.
+//  try to make ui as good as possible.......
